77a3d7b6a8869650d502c99fa6e509f7d3581c67
Jake Hitze <jedmaster@gmail.com>
2016-04-04 21:26:11 -0400

    more tests; fixed error when no data present

--- oak_weather_data_test.go
+++ oak_weather_data_test.go
@@ -43,3 +43,51 @@ func TestWeatherData_ambientPercentage_Low(t *testing.T) {
 		t.Errorf("WeatherData#ambientPercentage did return correct percentage. Got:%f", returned)
 	}
 }
+
+func TestNewWeatherData_withGoodData(t *testing.T) {
+	weatherJsonMap := make(map[string]interface{})
+	weatherData := "{\"temperature\" : 24.5, \"ambient\" : 200, \"pressure\" : 1048.23, \"humidity\" : 56}"
+
+	weatherJsonMap["data"] = weatherData
+
+	expected := WeatherData{24.5, 200, 1048.23, 56}
+
+	returned := NewWeatherData(weatherJsonMap)
+
+	if expected != *returned {
+		t.Errorf("TestNewWeatherData did return correct data. Got:%s, expected %s", returned.asString(), expected.asString())
+	}
+}
+
+func TestNewWeatherData_withNoData(t *testing.T) {
+	weatherJsonMap := make(map[string]interface{})
+
+	returned := NewWeatherData(weatherJsonMap)
+
+	if returned != nil {
+		t.Error("TestNewWeatherData did not return nil pointer.")
+	}
+}
+
+func TestNewWeatherData_withBadJSONinData(t *testing.T) {
+	weatherJsonMap := make(map[string]interface{})
+	weatherData := "\"temperature\" : 24.5, \"ambient\" : 200, \"pressure\" : 1048.23, \"humidity\" : 56"
+
+	weatherJsonMap["data"] = weatherData
+
+	returned := NewWeatherData(weatherJsonMap)
+
+	if returned != nil {
+		t.Error("TestNewWeatherData did not return nil pointer.")
+	}
+}
+
+func TestNewWeatherData_withBlankData(t *testing.T) {
+	weatherJsonMap := make(map[string]interface{})
+	weatherJsonMap["data"] = ""
+	returned := NewWeatherData(weatherJsonMap)
+
+	if returned != nil {
+		t.Error("TestNewWeatherData did not return nil pointer.")
+	}
+}
